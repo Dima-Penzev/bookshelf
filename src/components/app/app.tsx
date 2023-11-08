c1e16dfe19dd096e7f58e7fdc8e3e2a8829343c5
Dima <D130188@yandex.ru>
2023-11-08 10:50:37 +0300

    added initial styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./app.css";
+import { Loader } from "../loader/loader";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Routes>
-        <Route path="/" element={<h1>bookshelf</h1>} />
+        <Route path="/" element={<Loader />} />
       </Routes>
     </div>
   );

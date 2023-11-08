960bf3615fc6eadef102e662465113539f9cdfdd
Dima <D130188@yandex.ru>
2023-11-08 11:07:18 +0300

    added normalize

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./app.css";
-import { Loader } from "../loader/loader";
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Loader />} />
+        <Route path="/" element={<h1>bookshelf</h1>} />
       </Routes>
     </div>
   );
